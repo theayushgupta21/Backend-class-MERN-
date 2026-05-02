d48d1bcc5e4e4fe152428026987d6366099a9da1
Ayush Gupta <ayushgupta9510@gmail.com>
2026-05-02 23:50:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const ejs = require('ejs');
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    fs.readdir(`./files`, (err, files) => {
+        res.render("index", { files: files });
+    })
+})
+
+
+
+
+app.get('/file/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, file data) => {
+        res.render('show', { filename: req.params.filename, filedata: filedata });
+    })
+});
+
+app.post('/create', (req, res) => {
+    fs.writeFile(`./files/${req.body.title.split(' ').join(' ')}.txt`, req.body.details, function (err) {
+        res.redirect('/')
+    })
+})
+
+
+
+app.listen(3000);
